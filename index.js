50ac5e238e69d2dc83e74870eed453280d2e691a
delboy3450-pixel <delboy3450@gmail.com>
2026-02-27 14:39:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ Public channels must remain English.
 const RP_RULES = `
 ### 🚓 Roleplay Rules
 
-✅ **Realistic UK Roleplay**  
-UK-based realistic scenarios only.
+✅ **Realistic New York Roleplay**  
+New York-based realistic scenarios only.
 
 ✅ **No RDM / VDM**  
 Violence requires roleplay reasoning.
@@ -89,8 +89,8 @@ Staff may intervene anytime.
 function buildRulesPanel(type = "discord") {
   const embed = new EmbedBuilder()
     .setColor(0x2b2d31)
-    .setTitle("Boroughs of London Roleplay")
-    .setImage("https://images.unsplash.com/photo-1505761671935-60b3a7427bad?q=80&w=1200&auto=format&fit=crop")
+    .setTitle("New York State Roleplay")
+    .setImage("https://tse3.mm.bing.net/th/id/OIP.2u3eKNf46VqK795H-MPRoAHaCG?rs=1&pid=ImgDetMain&o=7&rm=3")
     .setDescription(type === "discord" ? DISCORD_RULES : RP_RULES)
     .setFooter({ text: "New York State Roleplay • Official Rules" });
 
@@ -267,6 +267,7 @@ client.on("interactionCreate", async interaction => {
 });
 
 client.login(TOKEN);
+
 
 
 
